26a23b4f0d37a18a6980514cb8a0341571554ed3
livinyaaa <113094911+livinyaaa@users.noreply.github.com>
2023-06-08 15:57:54 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]              INT           NOT NULL,
+    [UserName]        NVARCHAR (50) NOT NULL,
+    [EMail]           NVARCHAR (50) NOT NULL,
+    [Password]        NVARCHAR (50) NOT NULL,
+    [Confirmpassword] NVARCHAR (50) NOT NULL,
+    [Birthdate]       DATETIME      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
